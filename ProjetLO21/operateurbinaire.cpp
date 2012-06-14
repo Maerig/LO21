7d53d0dd2d968b3641b29d5d009dea307b45ea5b
Sigomars <marcelhaddad@gmail.com>
2012-06-14 21:51:25 +0200

    commit general

--- ProjetLO21/operateurbinaire.cpp
+++ ProjetLO21/operateurbinaire.cpp
@@ -13,61 +13,43 @@ void OperateurBinaire::Calculer(Pile* stack){
     const Rationnel* test4 = dynamic_cast<const Rationnel*>(dB);
     const Entier* test5 = dynamic_cast<const Entier*>(dA);
     const Entier* test6 = dynamic_cast<const Entier*>(dB);
-    /*
-    Reel dA(3.4);
-    Reel dB(11.3);
-
-    Reel* A1=&dA;
-    Reel* B1=&dB;
-
-    const Reel* test1 = dynamic_cast<const Reel*>(A1);
-    const Reel* test2 = dynamic_cast<const Reel*>(B1);
-
-
-    if ( test1 || test2 ) {
-
-            const Reel A=*test1;
-            const Reel B=*test2;
-            Reel C(A+B);
-            std::cout<<C.getVal();
-   }*/
 
 
     if ( test1 || test2 ) {
 
         switch (typeoperation) {
-        case (PLUS):
-{
-            const Reel A=*test1;
-            const Reel B=*test2;
-            Reel C(A+B);
+            case (PLUS):
+    {
+                const Reel A=*test1;
+                const Reel B=*test2;
+                Reel C(A+B);
 
-            Reel* dC=&C;
-            stack->empiler(dC);
-}
-            break;
-
-        case(MINUS):
-
-            break;
-
-        case(DIV):
-
-            break;
-
-        case (MULT):
-
-            break;
-
-        case(MODULO):
-
-            break;
-
-        case (POW):
-
-            break;
-
+                Reel* dC=&C;
+                stack->empiler(dC);
     }
+                break;
+
+            case(MINUS):
+
+                break;
+
+            case(DIV):
+
+                break;
+
+            case (MULT):
+
+                break;
+
+            case(MODULO):
+
+                break;
+
+            case (POW):
+
+                break;
+
+        }
     }
 
     else if ( test3 || test4 ) {
@@ -110,67 +92,66 @@ void OperateurBinaire::Calculer(Pile* stack){
     else if ( *test5 || *test6){
 
             switch (typeoperation) {
-            case (PLUS):
+                case (PLUS):
+
+                    {
+                            const Entier A=*test5;
+                            const Entier B=*test6;
+
+                            Entier C(A+B);
+                            Entier* dC=&C;
+                            stack->empiler(dC);
+                    }
+                    break;
+                case(MINUS):
+
                 {
                         const Entier A=*test5;
                         const Entier B=*test6;
 
-                        Entier C(A+B);
+                        Entier C(A-B);
                         Entier* dC=&C;
                         stack->empiler(dC);
                 }
-                break;
-            case(MINUS):
-            {
-                    const Entier A=*test5;
-                    const Entier B=*test6;
-
-                    Entier C(A-B);
-                    Entier* dC=&C;
-                    stack->empiler(dC);
-            }
-
-                break;
-
-            case(DIV):
-            {
-                    const Entier A=*test5;
-                    const Entier B=*test6;
-
-                    Entier C(A/B);
-                    Entier* dC=&C;
-                    stack->empiler(dC);
-            }
                     break;
 
+                case(DIV):
 
-                break;
+                {
+                        const Entier A=*test5;
+                        const Entier B=*test6;
 
-            case (MULT):
-            {
-                const Entier A=*test5;
-                const Entier B=*test6;
+                        Entier C(A/B);
+                        Entier* dC=&C;
+                        stack->empiler(dC);
+                }
+                    break;
 
-                Entier C(A*B);
-                Entier* dC=&C;
-                stack->empiler(dC);
+                case (MULT):
+
+                {
+                    const Entier A=*test5;
+                    const Entier B=*test6;
+
+                    Entier C(A*B);
+                    Entier* dC=&C;
+                    stack->empiler(dC);
+                }
+                    break;
+
+                case(MODULO):
+
+                {
+
+                }
+                    break;
+
+                case (POW):
+
+                    break;
+
             }
-                break;
 
-            case(MODULO):
-
-            {
-
-            }
-
-                break;
-
-            case (POW):
-
-                break;
-
-            }
-
-}
+    }
 
 }
